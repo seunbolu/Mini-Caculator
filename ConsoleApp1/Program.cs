7d4f83bb64b22b3ecfb866c156b88c0b246ba75a
Oluwaseun Atoyebi <seunbolu86@gmail.com>
2021-01-17 23:19:06 -0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum;
+            int secondNum;
+            int Sum;
+            char Oper;
+
+            Console.Write(" Enter the First Number:");
+            firstNum= int.Parse(Console.ReadLine());
+
+            Console.Write(" Enter the Operator:");
+            Oper = Convert.ToChar(Console.ReadLine());
+
+            Console.Write(" Enter the Second Number:");
+            secondNum = int.Parse(Console.ReadLine());
+
+            if (Oper == '+')
+            {
+                Sum = firstNum + secondNum;
+                Console.WriteLine("The Total =" + Sum);
+            }
+
+            else if (Oper == '-')
+            {
+                Sum = firstNum - secondNum;
+                Console.WriteLine("The Total =" + Sum);
+
+            }
+
+            else if (Oper == '/')
+            {
+                Sum = firstNum / secondNum;
+                Console.WriteLine("The Total =" + Sum);
+
+            }
+
+            else if (Oper == '*')
+            {
+                Sum = firstNum * secondNum;
+                Console.WriteLine("The Total =" + Sum);
+
+            }
+            else if (Oper == '%')
+            {
+                Sum = firstNum % secondNum;
+                Console.WriteLine("The Total =" +  Sum);
+
+            }
+
+
+        }
+    }
+}
